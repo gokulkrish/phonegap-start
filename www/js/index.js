049b35716a7db972997849bd1f036a9ab2be45e1
Shubham Verma <shubham.verman@gmail.com>
2014-06-26 16:29:37 +0530

    Added code for geolocation

--- www/js/index.js
+++ www/js/index.js
@@ -33,17 +33,12 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        alert('device ready');
+        
+        var suc = function(pos) { alert(pos.coords.latitude + ' ' + pos.coords.longitude); } 
+        var fail = function() {};
+        
+        navigator.geolocation.getCurrentPosition(suc, fail);
+        navigator.geolocation.watchPosition(suc, fail);
     }
 };
